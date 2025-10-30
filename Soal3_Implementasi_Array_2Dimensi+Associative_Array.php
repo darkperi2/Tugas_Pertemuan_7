9e710d16190ef4e2f5608583165bbaa348a22343
Ibrahim AL-TAQWA <elitebizarre@gmail.com>
2025-10-30 21:37:56 +0700

    membuat array

---
+++ Soal3_Implementasi_Array_2Dimensi+Associative_Array.php
@@ -0,0 +1,21 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>asoy</title>
+</head>
+
+
+<body>
+    <?php
+        $nilai = [
+            ["nama" => "jaka", "tugas" => 80, "uts" => 75, "uas" => 85],
+            ["nama" => "sinta", "tugas" => 90, "uts" => 88, "uas" => 92],
+            ["nama" => "budi", "tugas" => 70, "uts" => 65, "uas" => 78]
+        ]
+    ?>
+
+
+
+</body>
